02369da3e8682ca139877c2673f7944765d91ed2
bigboy97 <colin94153@gmail.com>
2017-10-03 17:35:07 -0400

    Added: page, elements, comments, ext.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
+//Configures Carousel
 $('.carousel').carousel({
     interval: 2000,
     pause: false
   });
 
+  //Specifiys a target to spy on
   $('body').scrollspy({ target: '#navbarSupportedContent' });
+
+  //Init tooltips
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
